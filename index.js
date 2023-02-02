adfdcb2db1713c3d57559598c67ac7ac4996c224
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-02 12:24:44 +0600

    connected with mongoose db

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 const port = process.env.PORT || 5000;
+
+/* CONNECTING DATABASE WITH MONGOOSE */
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.0hl8m1y.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("Successfully connected with database!"))
+  .catch((err) => console.log(err));
 
 /************* 404 ERROR HANDLER *************/
 app.use((_req, _res, next) => {
@@ -24,4 +39,4 @@ app.use((err, req, res, next) => {
   }
 });
 
-app.listen(port, () => console.log("Server is running on port - ", port));
+app.listen(port, () => console.log("Server is running on port -", port));
